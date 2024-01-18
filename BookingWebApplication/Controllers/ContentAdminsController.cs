ebc0e387bf2ed277b611e37d7ddc9de53ab9e48e
Pavlos Ntelis <66007133+PavlosNtelis@users.noreply.github.com>
2024-01-18 17:35:55 +0200

    CreateProvoli

---
+++ BookingWebApplication/Controllers/ContentAdminsController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookingWebApplication.Models;
+
+namespace BookingWebApplication.Controllers
+{
+    public class ContentAdminsController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public ContentAdminsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContentAdmins
+        public async Task<IActionResult> Index()
+        {
+            var appDBContext = _context.ContentAdmins.Include(c => c.User);
+            return View(await appDBContext.ToListAsync());
+        }
+
+        // GET: ContentAdmins/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Create
+        public IActionResult Create()
+        {
+            ViewData["UserName"] = new SelectList(_context.Users, "UserName", "UserName");
+            return View();
+        }
+
+        // POST: ContentAdmins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,UserName")] ContentAdmin contentAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contentAdmin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserName"] = new SelectList(_context.Users, "UserName", "UserName", contentAdmin.UserName);
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins.FindAsync(id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserName"] = new SelectList(_context.Users, "UserName", "UserName", contentAdmin.UserName);
+            return View(contentAdmin);
+        }
+
+        // POST: ContentAdmins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,UserName")] ContentAdmin contentAdmin)
+        {
+            if (id != contentAdmin.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contentAdmin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentAdminExists(contentAdmin.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserName"] = new SelectList(_context.Users, "UserName", "UserName", contentAdmin.UserName);
+            return View(contentAdmin);
+        }
+
+        // GET: ContentAdmins/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contentAdmin = await _context.ContentAdmins
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contentAdmin == null)
+            {
+                return NotFound();
+            }
+
+            return View(contentAdmin);
+        }
+
+        // POST: ContentAdmins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contentAdmin = await _context.ContentAdmins.FindAsync(id);
+            if (contentAdmin != null)
+            {
+                _context.ContentAdmins.Remove(contentAdmin);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentAdminExists(int id)
+        {
+            return _context.ContentAdmins.Any(e => e.Id == id);
+        }
+
+        // GET: ContentAdmins/CreateProvoli
+        public IActionResult CreateProvoli()
+        {
+            ViewBag.Cinemas = new SelectList(_context.Cinemas, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProvoli(Provoli provoli)
+        {
+            if (ModelState.IsValid)
+            {
+                var movie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieName == provoli.MoviesName);
+                var cinema = await _context.Cinemas.FindAsync(provoli.CinemasID);
+
+                if (movie != null && cinema != null)
+                {
+                    provoli.Movie = movie;
+                    provoli.Cinema = cinema;
+
+                    _context.Provoles.Add(provoli);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid movie or cinema.");
+                }
+            }
+
+            ViewBag.Cinemas = new SelectList(_context.Cinemas, "Id", "Name");
+            return View(provoli);
+        }
+    }
+}
